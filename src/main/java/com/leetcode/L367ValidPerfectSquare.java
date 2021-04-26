3477a1514ef18fee04e3f70602e0eb0fdb425d1c
jeremylai7 <1049024592@qq.com>
2021-04-26 17:58:30 +0800

    有效的完全平方数

---
+++ src/main/java/com/leetcode/L367ValidPerfectSquare.java
@@ -0,0 +1,37 @@
+package com.leetcode;
+
+import org.junit.Test;
+
+/**
+ * @Author: laizc
+ * @Date: Created in  2021-04-26
+ * @desc: 有效的完全平方数
+ */
+public class L367ValidPerfectSquare {
+
+	@Test
+	public void test() {
+		int x = 20;
+    	boolean valid = binary(x);
+		System.out.println(valid);
+	}
+
+	private boolean binary(int x) {
+		if (x <= 2) {
+			return true;
+		}
+		int left = 2,right = x/2,y,square;
+		while (left <= right) {
+			y = left + (right - left)/2;
+			square = y * y;
+			if (square == x) {
+				return true;
+			}else if (square > x) {
+				right = y - 1;
+			}else {
+				left = y + 1;
+			}
+		}
+		return false;
+	}
+}

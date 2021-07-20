c6773c1360038f9e6be8a9b942345755bec9c4b3
jeremylai7 <1049024592@qq.com>
2021-07-20 18:56:08 +0800

    三数之和

---
+++ src/main/java/com/leetcode/L15_3sum.java
@@ -0,0 +1,83 @@
+package com.leetcode;
+
+import javafx.scene.media.SubtitleTrack;
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * @Author: laizc
+ * @Date: Created in  2021-07-20
+ * @desc: 三数之和
+ */
+public class L15_3sum {
+
+	private int[] nums = {-1,0,1,2,-1,-4};
+
+
+	@Test
+	public void test() {
+		List<List<Integer>> lists = test(nums);
+		print(lists);
+	}
+
+	private List<List<Integer>> test(int[] nums) {
+		List<List<Integer>> lists = new ArrayList<>();
+		Arrays.sort(nums);
+		//for循环 + 双指针
+		for (int i = 0; i < nums.length; i++) {
+			if (i > 0 && nums[i] == nums[i-1]) {
+				continue;
+			}
+			int target = -nums[i];
+			int left = i +1,right = nums.length - 1;
+			while (left < right) {
+				int sum = nums[left] + nums[right];
+				if (sum == target) {
+					List<Integer> list = new ArrayList<>();
+					list.add(nums[i]);
+					list.add(nums[left]);
+					list.add(nums[right]);
+					lists.add(list);
+					break;
+				} else if (sum < target) {
+					left++;
+				} else {
+					right--;
+				}
+			}
+		}
+		return lists;
+
+
+
+		/*for (int i = 0; i < nums.length; i++) {
+			int first = nums[i];
+			Map<Integer,Integer> map = new HashMap<>();
+			for (int j = i+1; j < nums.length; j++) {
+				int second = nums[j];
+				int sum = 0 - first - second;
+				if (map.containsKey(sum)) {
+					System.out.println(" " + i + " "+ j + " " + map.get(sum));
+					List<Integer> list = new ArrayList<>();
+					list.add(nums[i]);
+					list.add(nums[j]);
+					list.add(sum);
+					lists.add(list);
+				} else {
+					map.put(nums[j],j);
+				}
+			}
+		}
+		return lists;*/
+	}
+
+	private void print(List<List<Integer>> lists) {
+		for (List<Integer> list: lists) {
+			for (Integer integer : list) {
+				System.out.print(integer + " ");
+			}
+			System.out.println();
+		}
+	}
+}

0187fb43e0a866569905ea8f5aa6d11bde48f69b
jeremylai7 <1049024592@qq.com>
2021-08-02 21:18:44 +0800

    添加单链表

---
+++ src/main/java/com/leetcode/ListNode.java
@@ -0,0 +1,22 @@
+package com.leetcode;
+
+
+/**
+ * @author: laizc
+ * @date: created in 2021/8/2
+ * @desc:
+ **/
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+
+}

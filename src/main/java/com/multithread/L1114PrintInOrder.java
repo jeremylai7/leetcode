5104da17d5987d80df520e1ab4dc95938fdfc214
jeremylai7 <1049024592@qq.com>
2021-08-09 10:44:36 +0800

    按序打印

---
+++ src/main/java/com/multithread/L1114PrintInOrder.java
@@ -0,0 +1,67 @@
+package com.multithread;
+
+import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @Author: laizc
+ * @Date: Created in  2021-08-09
+ * @desc: 按序打印
+ *
+ * 示例 1:
+
+输入: [1,2,3]
+输出: "firstsecondthird"
+解释:
+有三个线程会被异步启动。
+输入 [1,2,3] 表示线程 A 将会调用 first() 方法，线程 B 将会调用 second() 方法，线程 C 将会调用 third() 方法。
+正确的输出是 "firstsecondthird"。
+ */
+public class L1114PrintInOrder {
+
+	@Test
+	public void test() throws InterruptedException {
+		Foo foo = new Foo();
+		foo.first(() -> {
+
+		});
+		foo.second(() -> {
+
+		});
+
+	}
+
+
+
+
+}
+
+class Foo{
+
+	private static AtomicInteger atomicInteger = new AtomicInteger();
+
+	public Foo() {}
+
+	public void first(Runnable printFirst) throws InterruptedException{
+		printFirst.run();
+		atomicInteger.incrementAndGet();
+	}
+
+	public void second(Runnable printSecond) throws InterruptedException{
+		while (atomicInteger.get() != 1) {
+
+		}
+		printSecond.run();
+		atomicInteger.incrementAndGet();
+	}
+
+	public void third(Runnable printThird) throws InterruptedException{
+		while (atomicInteger.get() != 2) {
+
+		}
+		printThird.run();
+		atomicInteger.incrementAndGet();
+	}
+
+}

d4f710b89df17ea19b0d913c38c26c13284d1f70
jeremylai7 <1049024592@qq.com>
2022-01-19 17:13:40 +0800

    乘积最大子数组

--- src/main/java/com/leetcode/L152MaximumProductSubarray.java
+++ src/main/java/com/leetcode/L152MaximumProductSubarray.java
@@ -47,4 +47,21 @@ public class L152MaximumProductSubarray {
 		}
 		return max;
 	}
+
+	public int maxProduct2(int[] nums) {
+		int n = nums.length;
+		if (n == 0) {
+			return 0;
+		}
+		int dpMax = nums[0];
+		int dpMin = nums[0];
+		int max = nums[0];
+		for (int i = 1; i < n; i++) {
+			int preMax = dpMax;
+			dpMax = Math.max(dpMin * nums[i], Math.max(dpMax * nums[i],nums[i]));
+			dpMin = Math.min(dpMin * nums[i], Math.min(preMax * nums[i],nums[i]));
+			max = Math.max(max,dpMax);
+		}
+		return max;
+	}
 }
